c91a7abbcc7bf231ef9709a44f69207661fa0b12
Harshvardhan Chittora <chittora.harshvardhan@gmail.com>
2017-03-26 10:41:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ if(!empty($_GET['status'])){
     <title>Bank Statement</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/">
+    <script src="js/js1.js"></script>
+    <script src="js/js2.js"></script>
     <style type="text/css">
         .panel-heading a{float: right;}
         #importFrm{margin-bottom: 20px;display: none;}
