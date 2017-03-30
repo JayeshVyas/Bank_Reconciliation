9005a997a2d051f985549c75630a6ca8cdcc2228
Harshvardhan Chittora <chittora.harshvardhan@gmail.com>
2017-03-30 09:37:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,98 +1,51 @@
-<?php
-//load the database configuration file
-include 'DBConfig.php';
-
-if(!empty($_GET['status'])){
-    switch($_GET['status']){
-        case 'succ':
-            $statusMsgClass = 'alert-success';
-            $statusMsg = 'Statement has been uploaded successfully.';
-            break;
-        case 'err':
-            $statusMsgClass = 'alert-danger';
-            $statusMsg = 'Some problem occurred, please try again.';
-            break;
-        case 'invalid_file':
-            $statusMsgClass = 'alert-danger';
-            $statusMsg = 'Please upload a valid CSV file.';
-            break;
-        default:
-            $statusMsgClass = '';
-            $statusMsg = '';
-    }
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Bank Statement</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/style1.css">
-    <script src="js/js1.js"></script>
-    <script src="js/js2.js"></script>
-    <style type="text/css">
-        .panel-heading a{float: right;}
-        #importFrm{margin-bottom: 20px;display: none;}
-        #importFrm input[type=file] {display: inline;}
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h2>Bank Statement</h2>
-        <?php if(!empty($statusMsg)){
-            echo '<div class="alert '.$statusMsgClass.'">'.$statusMsg.'</div>';
-        } ?>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            Statement
-            <a href="javascript:void(0);" onclick="$('#importFrm').slideToggle();">Import Another Statement</a>
+<html>
+    
+    <head>
+        <title>Bank Reconciliation</title>
+        <link rel="stylesheet" type="text/css" href="css/navigation.css">
+        
+        <script>
+            function myFunction()
+            {
+                alert("Hello");
+            }
+        </script>
+        <style>
+            a:hover{
+                color: white;
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    
+    <body>
+        
+        <div>
+            <ul>
+                <li class="dropdown">
+                    <span href="Home.php" class="dropbtn" onclick="myFunction();">Home</span>
+                </li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Statements</a>
+                    <div class="dropdown-content">
+                        <a href="bank_statement.php">Import Bank Statement</a>
+                        <a href="internal_statement.php">Import Internal Statement</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Reconciled Records</a>
+                </li>
+                <li class="dropdown">
+                    <a href="managereconcile.php" class="dropbtn">Unreconciled Records</a>
+                </li>
+                <li class="dropdown" style="float:right">
+                        <a href="javascript:void(0)" class="dropbtn">Logout</a>
+                </li>
+            </ul>
         </div>
-        <div class="panel-body">
-            <form action="importData.php" method="post" enctype="multipart/form-data" id="importFrm">
-                Select Bank <br />
-                <input type="radio" name="selectBank" value="HDFC" checked> HDFC &nbsp;&nbsp;
-                <input type="radio" name="selectBank" value="SBI"> SBI &nbsp;&nbsp;
-                <br />
-                <input type="file" name="file" />
-                <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">
-            </form>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Particulars</th>
-                        <th>Cheque/Ref. No.</th>
-                        <th>Deposits</th>
-                        <th>Withdrawals</th>
-                        <th>Bank Name</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    //get rows query
-                    $query = $db->query("SELECT * FROM bankstatement");
-                    if($query->num_rows > 0){ 
-                        while($row = $query->fetch_assoc()){
-                        ?>
-                    <tr>
-                        <td><?php echo $row['cdate']; ?></td>
-                        <td><?php echo $row['particulars']; ?></td>
-                        <td><?php echo $row['reference']; ?></td>
-                        <td><?php echo $row['deposits']; ?></td>
-                        <td><?php echo $row['withdrawals']; ?></td>
-                        <td><?php echo $row['bankname']; ?></td>
-                        <td><?php echo $row['status'];?></td>
-                    </tr>
-                    <?php } }else{ ?>
-                    <tr><td colspan="8">No record(s) found.....</td></tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+        
+        <div id="mainDiv">
         </div>
-    </div>
-</div>
-
-</body>
+        
+    </body>
 </html>
